1c2dd0bd3cfa1f061f622af3767c65f8017393e4
Java Coding <103120323+javacoder61@users.noreply.github.com>
2024-12-25 14:12:30 +0300

    Create aiven.php

---
+++ aiven.php
@@ -0,0 +1,22 @@
+<?php
+
+$uri = "mysql://avnadmin:AVNS_z3JFM2zwSiYjCLfzjiA@mysql-db101-meals.d.aivencloud.com:28690/defaultdb?ssl-mode=REQUIRED";
+
+$fields = parse_url($uri);
+
+// build the DSN including SSL settings
+$conn = "mysql:";
+$conn .= "host=" . $fields["host"];
+$conn .= ";port=" . $fields["port"];;
+$conn .= ";dbname=defaultdb";
+$conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
+
+try {
+  $db = new PDO($conn, $fields["user"], $fields["pass"]);
+
+  $stmt = $db->query("SELECT VERSION()");
+  print($stmt->fetch()[0]);
+} catch (Exception $e) {
+  echo "Error: " . $e->getMessage();
+  echo $fields["host"];
+}
